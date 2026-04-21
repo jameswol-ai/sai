fe11778756db354aab3d864af9f79a24e26b5583
James Wol <james.wol@petalmail.com>
2026-04-21 23:13:27 +0200

    Create __init__.py

---
+++ tests/integration/__init__.py
@@ -0,0 +1,15 @@
+"""
+Integration tests package initializer.
+
+This file makes the tests/integration/ directory a proper Python package.
+It can be used to:
+- Expose shared integration test utilities
+- Configure test-wide constants
+- Provide metadata for the integration test suite
+"""
+
+# Optional: import shared utilities if you create tests/utils.py
+# from ..utils import *  # noqa: F401,F403
+
+__version__ = "0.1.0"
+__all__ = []

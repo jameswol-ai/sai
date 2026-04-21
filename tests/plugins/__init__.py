0cc1e21212f428327d89db8a1413f952a4466f4c
James Wol <james.wol@petalmail.com>
2026-04-21 23:17:19 +0200

    Create __init__.py

---
+++ tests/plugins/__init__.py
@@ -0,0 +1,15 @@
+"""
+Plugins tests package initializer.
+
+This file makes the tests/plugins/ directory a proper Python package.
+It can be used to:
+- Expose shared plugin test utilities
+- Configure plugin test-wide constants
+- Provide metadata for the plugin test suite
+"""
+
+# Optional: import shared utilities if you create tests/utils.py
+# from ..unit.test_utils import *  # noqa: F401,F403
+
+__version__ = "0.1.0"
+__all__ = []

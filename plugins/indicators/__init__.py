f221aa2ea7b742138ab247a43820157fd4544cdf
James Wol <james.wol@petalmail.com>
2026-04-21 11:18:45 +0200

    Create __init__.py

---
+++ plugins/indicators/__init__.py
@@ -0,0 +1,20 @@
+# plugins/indicators/__init__.py
+"""
+Indicators package initializer.
+
+This file exposes all technical indicator functions for easy import.
+Usage:
+    from plugins.indicators import sma, rsi, bollinger_bands, macd
+"""
+
+from .moving_average import sma
+from .rsi import rsi
+from .bollinger_bands import bollinger_bands
+from .macd import macd
+
+__all__ = [
+    "sma",
+    "rsi",
+    "bollinger_bands",
+    "macd",
+]

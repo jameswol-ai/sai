554864e88df0186873ef463fbcc3a66e3c92a560
James Wol <james.wol@petalmail.com>
2026-04-21 23:01:46 +0200

    Create __init__.py

---
+++ tests/unit/__init__.py
@@ -0,0 +1,15 @@
+"""
+Unit tests package initializer.
+
+This file makes the tests/unit/ directory a proper Python package.
+It can be used to:
+- Expose shared unit test utilities
+- Configure test-wide constants
+- Provide metadata for the unit test suite
+"""
+
+# Optional: import shared utilities if you create tests/utils.py
+# from ..utils import *  # noqa: F401,F403
+
+__version__ = "0.1.0"
+__all__ = []

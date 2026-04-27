f94d6e34b7c09f6b7641d5791d06364cc7abc750
James Wol <james.wol@petalmail.com>
2026-04-27 18:06:20 +0200

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,6 @@
-# Marks 'sai.bot' as a package
+# sai/bot/__init__.py
+"""
+Bot subpackage initializer.
+Exposes key functions for import.
+"""
+from .main import run_bot, get_data, decide_action, SimpleModel

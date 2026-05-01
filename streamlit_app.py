87a71b3f539c7a359df45598853d297d1c178c26
James Wol <james.wol@petalmail.com>
2026-05-02 01:54:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pickle
 import random
 import logging
-from binance import sai.configs.binance
+from sai.configs.binance import binance
 
 # --- Logging Setup ---
 logging.basicConfig(filename="workflow.log", level=logging.INFO,

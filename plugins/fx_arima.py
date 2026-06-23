c4b1ca87ad3e7b6fb1914caf3fa20445d986d405
James Wol <james.wol@petalmail.com>
2026-06-23 23:00:30 +0300

    Add ARIMA forecasting function in fx_arima.py

---
+++ plugins/fx_arima.py
@@ -0,0 +1,9 @@
+# plugins/prediction/fx_arima.py
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
+def forecast(series, steps=7):
+    model = ARIMA(series, order=(1,1,1))
+    model_fit = model.fit()
+    preds = model_fit.forecast(steps=steps)
+    return preds.tolist()
